ef73cf47167f8a6de0af5ff7f376096bc4700ce1
Masaki-Shinkawa <masaki.shinkawa@adglobe.co.jp>
2023-02-07 01:46:39 +0900

    feat: Googleログインの実装

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,22 @@
 <template>
-  <div class="text-xl bg-red-500">
-    aaa
+  <div>
+    <div
+      id="g_id_onload"
+      data-client_id="989412605567-djerugsq4f00ed90plegrt2i3lklf92d.apps.googleusercontent.com"
+      data-context="signin"
+      data-login_uri="http://localhost:3000"
+      data-auto_select="true"
+      data-itp_support="true"
+    >
+      aaaaa
+    </div>
   </div>
 </template>
+
+<script setup lang="ts">
+useHead({
+  script: [
+    { src: "https://accounts.google.com/gsi/client", async: true, defer: true },
+  ],
+});
+</script>
